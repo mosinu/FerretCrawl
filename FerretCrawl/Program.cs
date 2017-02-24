c86e5e602d9cd17060a8f8461243259b784d0317
Will <Will@WILL>
2017-02-23 23:39:04 -0500

    client change

--- FerretCrawl/Program.cs
+++ FerretCrawl/Program.cs
@@ -5,13 +5,16 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Diagnostics;
 using System.Net;
+using System.IO;
 
 namespace FerretCrawl
 {
     class Program
     {
-        static void Main()
+        static void Main(string[] args)
         {
+            //PrintDisclaimer();
+
             // Create web client
             using (WebClient client = new WebClient())
             {
@@ -23,18 +26,52 @@ namespace FerretCrawl
                     //".NET CLR 1.1.4322; .NET CLR 2.0.50727)";
 
                 // Accept-encoding headers.
-                client.Headers["Accept-Encoding"] = "gzip";
+                client.Headers["Accept-Encoding"] = "html";
+                client.Headers["Accept-Encoding"] = "text";
 
                 // Download data.
-                byte[] arr = client.DownloadData("http://www.mosinu.com/");
+                byte[] arr = client.DownloadData("http://blog.mosinu.com/");
+                string sTr = client.DownloadString("http://blog.mosinu.com/");
 
                 // Get response header.
                 string contentEncoding = client.ResponseHeaders["Content-Encoding"];
 
                 // Write values.
-                Console.WriteLine("--- WebClient result ---");
-                Console.WriteLine(arr.Length);
+                //Console.WriteLine("--- WebClient result ---");
+                PrintRedAttentionText(" *** Ferret result *** ");
+                foreach (LinkItem i in LinkFinder.Find(sTr))
+                {
+                    Console.WriteLine(arr.Length);
+                    client.DownloadDataAsync(i);
+                    //Debug.WriteLine(i);
+                    Console.WriteLine(i);
+                }
+                Console.ReadLine();
             }
+        }
+
+        // Add a disclaimer
+        private static void PrintDisclaimer()
+        {
+            PrintAttentionText("Ferret");
+        }
+
+        // Set console color to yellow
+        private static void PrintAttentionText(string text)
+        {
+            ConsoleColor originalColor = System.Console.ForegroundColor;
+            System.Console.ForegroundColor = ConsoleColor.Yellow;
+            System.Console.WriteLine(text);
+            System.Console.ForegroundColor = originalColor;
+        }
+
+        // Set console color to red
+        private static void PrintRedAttentionText(string text)
+        {
+            ConsoleColor originalColor = System.Console.ForegroundColor;
+            System.Console.ForegroundColor = ConsoleColor.Red;
+            System.Console.WriteLine(text);
+            System.Console.ForegroundColor = originalColor;
         }
     }
 }
