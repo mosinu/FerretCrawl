99e509a8064537c4b8f661d0f15887b7a2758830
Will <Will@WILL>
2017-02-23 22:06:51 -0500

    agent change

--- FerretCrawl/Program.cs
+++ FerretCrawl/Program.cs
@@ -17,9 +17,10 @@ namespace FerretCrawl
             {
                 // Set user agent.
                 client.Headers["User-Agent"] =
-                    "Mozilla/4.0 (Compatible; Windows NT 5.1; MSIE 6.0) " +
-                    "(compatible; MSIE 6.0; Windows NT 5.1; " +
-                    ".NET CLR 1.1.4322; .NET CLR 2.0.50727)";
+                    "Mozilla/5.0 (Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko";
+                    //"Mozilla/4.0 (Compatible; Windows NT 5.1; MSIE 6.0) " +
+                    //"(compatible; MSIE 6.0; Windows NT 5.1; " +
+                    //".NET CLR 1.1.4322; .NET CLR 2.0.50727)";
 
                 // Accept-encoding headers.
                 client.Headers["Accept-Encoding"] = "gzip";
